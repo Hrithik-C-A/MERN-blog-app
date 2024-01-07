1634ca60c8b48e53e01b32b59d32dc6c9ec38510
Hrithik C A <hrithikca@gmail.com>
2024-01-07 18:42:52 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+//Entry point
+
+app.get('/', (req, res) => {
+    res.send('Server is running!')
+})
+
+app.listen(port, () => {
+    console.log(`App is running on ${port}`);
+})
