236dfb9299965b385da0421029dda3e9e7517f8c
Hrithik C A <hrithikca@gmail.com>
2024-01-15 08:59:25 +0000

    mock data

--- server/data/posts.js
+++ server/data/posts.js
@@ -2,18 +2,21 @@ const postsData = [
     {
       title: "Introduction to MongoDB",
       content: "MongoDB is a NoSQL database...",
+      image: 'Image_url',
       category: "your_category_id",
       author: "your_user_id",
     },
     {
       title: "Traveling to Beautiful Places",
       content: "I recently traveled to amazing destinations...",
+      image: 'Image_url',
       category: "your_category_id",
       author: "your_user_id",
     },
     {
       title: "Healthy Habits for a Better Life",
       content: "Maintaining a healthy lifestyle is important...",
+      image: 'Image_url',
       category: "your_category_id",
       author: "your_user_id",
     },
